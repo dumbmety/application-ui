8a9f84529d7e822e5ceee55cddbd54801a3ebd99
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-02-09 21:14:37 +0330

    feat: custom scrollbar like macos

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
+import SimpleBar from 'simplebar-react'
+import 'simplebar/dist/simplebar.min.css'
+
 import Header from '../components/common/Header'
 import Gallery from '../components/common/Gallery/Gallery'
 
 const App = () => {
   return (
-    <div className="container mx-auto py-12 text-gray-800">
-      <Header />
-      <Gallery />
-    </div>
+    <SimpleBar className="h-screen">
+      <div className="container mx-auto py-12 text-gray-800">
+        <Header />
+        <Gallery />
+      </div>
+    </SimpleBar>
   )
 }
 
