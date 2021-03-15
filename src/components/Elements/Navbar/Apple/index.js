a4526ce29a3810933ae0fbd1745281f69a438ab2
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-16 00:08:30 +0330

    refactor: apple classes

--- src/components/Elements/Navbar/Apple/index.js
+++ src/components/Elements/Navbar/Apple/index.js
@@ -1,68 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HiOutlineMenuAlt4 } from 'react-icons/hi';
 import { AiFillApple, AiOutlineSearch, AiOutlineShopping } from 'react-icons/ai';
 
+import './index.css';
 import Card from '../../../Layout/Card';
 
 const Apple = () => {
   return (
     <>
       <Card title="Apple">
-        <header
-          className="flex items-center w-full h-12 text-white select-none"
-          style={{ background: 'rgba(0,0,0,0.8)' }}
-        >
-          <nav className="w-full px-3 mx-auto md:px-0 md:w-9/12">
-            <ul className="items-center justify-between hidden md:flex">
-              <li>
-                <a href="#">
-                  <AiFillApple size="1.5rem" className="opacity-70 hover:opacity-100" />
-                </a>
+        <header className="apple-header">
+          <nav className="apple-desktop-nav">
+            <ul className="apple-desktop-items">
+              <li className="apple-desktop-item">
+                <Link to="/navbar">
+                  <AiFillApple size="1.5rem" />
+                </Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">Mac</a>
+              <li className="apple-desktop-item">
+                <Link to="/navbar">Mac</Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">iPad</a>
+              <li className="apple-desktop-item">
+                <Link to="/navbar">iPad</Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">iPhone</a>
+              <li className="apple-desktop-item">
+                <Link to="/navbar">iPhone</Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">Watch</a>
+              <li className="apple-desktop-item">
+                <Link to="/navbar">Watch</Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">Music</a>
+              <li className="apple-desktop-item">
+                <Link to="/navbar">Music</Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">Support</a>
+              <li className="apple-desktop-item">
+                <Link to="/navbar">Support</Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">
+              <li className="apple-desktop-item">
+                <Link to="/navbar">
                   <AiOutlineSearch size="1.5rem" />
-                </a>
+                </Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">
+              <li className="apple-desktop-item">
+                <Link to="/navbar">
                   <AiOutlineShopping size="1.5rem" />
-                </a>
+                </Link>
               </li>
             </ul>
-            <ul className="flex items-center justify-between md:hidden">
-              <li className="flex items-center">
-                <button className="opacity-70 hover:opacity-100 focus:outline-none">
+            <ul className="apple-mobile-nav">
+              <li className="apple-mobile-menu-button">
+                <button>
                   <HiOutlineMenuAlt4 size="1.5rem" />
                 </button>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">
+              <li className="apple-mobile-item">
+                <Link to="/navbar">
                   <AiFillApple size="1.5rem" />
-                </a>
+                </Link>
               </li>
-              <li className="opacity-70 hover:opacity-100">
-                <a href="#">
+              <li className="apple-mobile-item">
+                <Link to="/navbar">
                   <AiOutlineShopping size="1.5rem" />
-                </a>
+                </Link>
               </li>
             </ul>
           </nav>
