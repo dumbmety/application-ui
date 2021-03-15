e368a3b41295bdcdb61c80ae3928273508b6bc3d
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-03-15 23:49:33 +0330

    feat: responsive apple navbar

--- src/components/Elements/Navbar/Apple/index.js
+++ src/components/Elements/Navbar/Apple/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
 import { AiFillApple, AiOutlineSearch, AiOutlineShopping } from 'react-icons/ai';
 
 import Card from '../../../Layout/Card';
@@ -11,8 +12,8 @@ const Apple = () => {
           className="flex items-center w-full h-12 text-white select-none"
           style={{ background: 'rgba(0,0,0,0.8)' }}
         >
-          <nav className="w-9/12 mx-auto">
-            <ul className="flex items-center justify-between">
+          <nav className="w-full px-3 mx-auto md:px-0 md:w-9/12">
+            <ul className="items-center justify-between hidden md:flex">
               <li>
                 <a href="#">
                   <AiFillApple size="1.5rem" className="opacity-70 hover:opacity-100" />
@@ -39,6 +40,23 @@ const Apple = () => {
               <li className="opacity-70 hover:opacity-100">
                 <a href="#">
                   <AiOutlineSearch size="1.5rem" />
+                </a>
+              </li>
+              <li className="opacity-70 hover:opacity-100">
+                <a href="#">
+                  <AiOutlineShopping size="1.5rem" />
+                </a>
+              </li>
+            </ul>
+            <ul className="flex items-center justify-between md:hidden">
+              <li className="flex items-center">
+                <button className="opacity-70 hover:opacity-100 focus:outline-none">
+                  <HiOutlineMenuAlt4 size="1.5rem" />
+                </button>
+              </li>
+              <li className="opacity-70 hover:opacity-100">
+                <a href="#">
+                  <AiFillApple size="1.5rem" />
                 </a>
               </li>
               <li className="opacity-70 hover:opacity-100">
